84741d68095b9bb147854854d49753ced5e8b588
Philipp Deppenwiese <zaolin@das-labor.org>
2016-02-17 23:44:20 +0100

    Restructure and remove old stuff

--- dos/dataio.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BRMBAR_DATAIO_H_
-#define _BRMBAR_DATAIO_H_
-
-extern int modify_credit(name, price);
-
-#endif
--- mbed/request.h
+++
@@ -1,6 +0,0 @@
-#ifndef __REQUEST__H
-#define __REQUEST__H
-
-bool request(const char *arg, char *response);
-
-#endif
